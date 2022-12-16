6332540562f3be1820bc34fefc9970f869c68295
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-12-16 09:49:16 -0600

    changed the error and added an if statement to see if wither query returned a value or not

--- php/Relation.php
+++ php/Relation.php
@@ -14,8 +14,14 @@
             $query = $this->db->prepare($sql);
             $query->execute(array(':from' => $from, ':to' => $to));
 
-            if (is_array($query->fetch(PDO::FETCH_ASSOC))) {
-                $this->error = 'You have already sent a request to this user.';
+            /**
+             * 
+             * if (is_array($query->fetch(PDO::FETCH_ASSOC))) {
+             *  $this->error = 'You have already sent a request to this user.';
+             * return false;
+             * } */
+            if ($query->rowCount() == 1) {
+                $this->error = 'You are already send a request to this usr.';
                 return false;
             }
 
@@ -26,8 +32,14 @@
                     status='p' AND from=:to AND to=:from
                 )";
 
+            /*
             if (is_array($query->fetch(PDO::FETCH_ASSOC))) {
                 $this->error = 'Your request is pending.';
+                return false;
+            }*/
+
+            if ($query->rowCount() == 1) {
+                $this->error = 'Your request is pending';
                 return false;
             }
 
@@ -70,4 +82,4 @@
 
             return true;
         }
-    }
+    } 
