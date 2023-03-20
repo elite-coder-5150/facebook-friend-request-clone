665ab8f59e645d8a26ea25fae89e05c594df492f
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-03-20 02:44:53 -0500

    added some configuration

--- php/Database.php
+++ php/Database.php
@@ -1,11 +1,18 @@
 <?php
 	class Database extends PDO {
-		protected $inst;
-		protected $dsn;
+		protected static $inst;
+		protected static $dsn;
+        protected static $user;
+        protected static $pass;
 
 		public function __construct() {
+            parent::__construct();
+
+            self::$user = 'root';
+            self::$pass = 'root';
 			self::$dsn = 'mysql:host=localhost;dbname=kaylios';
-			self::$inst = new PDO(self::$dsn, 'root', 'root');
+
+			self::$inst = new PDO(self::$dsn, self::$user, self::$pass);
 			self::$inst->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}
 	}
